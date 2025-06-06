1e85434eaa04bedca87dba7c316cc332b9701ea2
Keren <keren.courtois@etudiant.univ-rennes1.fr>
2025-06-06 12:20:15 +0200

    merge

--- src/app/controller/Main.java
+++ src/app/controller/Main.java
@@ -1,12 +1,5 @@
 package app.controller;
 
-<<<<<<< HEAD
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-=======
-
 import java.io.IOException;
 
 import app.ui.FenAjouter;
@@ -15,32 +8,23 @@ import app.ui.FenTable;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
 	static private FenTable fTable;
 	static private FenAjouter fAjout;
 	static private FenPlan fPlan;
+	
 	
 	public void start(Stage maFenetre) throws IOException{
 		fPlan = new FenPlan();
 		fTable = new FenTable();
 		fAjout = new FenAjouter();
-		fPlan.show();
+		test fentest = new test();
+		fentest.show();
 	}
->>>>>>> 3a912cd35bd964469fbe75460531701b926630af
 
 	public static void main(String[] args) {
 		Application.launch();
 	}
-<<<<<<< HEAD
-
-	@Override
-	public void start(Stage arg0) throws Exception {
-		test fentest = new test();
-		fentest.show();
-		
-	}
-}
-=======
 	
 ///////////////////////
 // gestion des fenêtres
@@ -69,5 +53,3 @@ public class Main extends Application{
 	
 	
 }
-
->>>>>>> 3a912cd35bd964469fbe75460531701b926630af
